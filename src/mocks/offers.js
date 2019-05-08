62ebd6d10cfbc6ca231cc3c8f0b0f500c4cdb644
Vitaly Krupin <vitalykrupin@gmail.com>
2019-05-08 13:20:25 +0500

    4 offers' structure were made

---
+++ src/mocks/offers.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    name: `Beautiful, luxurious apartment at great location`,
+    type: `Apartment`,
+    image: `img/apartment-01.jpg`,
+    price: {
+      value: `120`,
+      currency: `euro`
+    },
+    rate: 93,
+    isBookmarked: true,
+    isPremium: true
+  },
+  {
+    name: `Wood and stone place`,
+    type: `Private room`,
+    image: `img/room.jpg`,
+    price: {
+      value: `80`,
+      currency: `euro`
+    },
+    rate: 80,
+    isBookmarked: false,
+    isPremium: false
+  },
+  {
+    name: `Canal View Prinsengracht`,
+    type: `Apartment`,
+    image: `img/apartment-02.jpg`,
+    price: {
+      value: `132`,
+      currency: `euro`
+    },
+    rate: 80,
+    isBookmarked: true,
+    isPremium: false
+  },
+  {
+    name: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    image: `img/apartment-03.jpg`,
+    price: {
+      value: `180`,
+      currency: `euro`
+    },
+    rate: 100,
+    isBookmarked: false,
+    isPremium: true
+  },
+];
