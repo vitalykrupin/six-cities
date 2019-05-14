05557fb9f55b608bf451abe3847dd7663701a5eb
Vitaly Krupin <vitalykrupin@gmail.com>
2019-05-15 00:55:04 +0500

    some changes

--- src/components/card-board/card-board.jsx
+++ src/components/card-board/card-board.jsx
@@ -14,11 +14,7 @@ class CardBoard extends Component {
       <div className="cities__places-list places__list tabs__content">
         {offers.map((card, i) => <PlaceCard
           key={i}
-          title={card.title}
-          type={card.type}
-          price={card.price}
-          rate={card.rate}
-          image={card.image}
+          {...card}
         />)}
       </div>
     );
