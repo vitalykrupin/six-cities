186da18153bdb5bdbb27dd0a866a41f66b691b6e
Vitaly Krupin <vitalykrupin@gmail.com>
2019-05-30 10:19:12 +0500

    api config was created

---
+++ src/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const createAPI = (dispatch) => {
+  const api = axios.create({
+    baseURL: `https://es31-server.appspot.com/six-cities`,
+    timeout: 1000 * 5,
+    withCredentials: true
+  });
+};
