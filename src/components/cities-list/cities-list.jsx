a44f33eca71a9c13d7c6ab42336f516a6c418e01
Vitaly Krupin <vitalykrupin@gmail.com>
2019-05-30 09:57:02 +0500

    citiesList now wrapped

--- src/components/cities-list/cities-list.jsx
+++ src/components/cities-list/cities-list.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import withHoverItem from '../../hocs/with-hover-item/with-hover-item';
 
-const CitiesList = ({cities, city, onCityClick}) => {
+const CitiesList = ({cities, city, onCityClick, setHoverItem}) => {
   return (
     <ul className={classNames(`locations__list`, `tabs__list`)}>
       {cities.map((it) =>
         <li className={`locations__item`} key={`city-${it}`}>
-          <a onClick={() => onCityClick(it)}
-            className={classNames(
-                `locations__item-link`,
-                `${it === city ? `tabs__item tabs__item--active` : `tabs__item`}`
-            )}
+          <a onClick={() => {
+            onCityClick(it);
+            setHoverItem(it);
+          }} className={classNames(
+              `locations__item-link`,
+              `${it === city ? `tabs__item tabs__item--active` : `tabs__item`}`
+          )}
           >
             <span>{it}</span>
           </a>
@@ -24,7 +27,8 @@ const CitiesList = ({cities, city, onCityClick}) => {
 CitiesList.propTypes = {
   cities: PropTypes.arrayOf(PropTypes.string),
   city: PropTypes.string,
-  onCityClick: PropTypes.func
+  onCityClick: PropTypes.func,
+  setHoverItem: PropTypes.func
 };
 
-export default CitiesList;
+export default withHoverItem(CitiesList);
