03cd4ba0b4d99c6e7e92ca7ec8310ad46547f7c4
Vitaly Krupin <vitalykrupin@gmail.com>
2019-05-30 10:20:51 +0500

    creatStore was rebuild with middleware

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import thunk from 'redux-thunk';
+import compose from 'recompose';
 
 import App from './components/app/app';
+import {createAPI} from './api';
 import {offers} from '../src/mocks/offers';
-import {reducer} from "./reducer";
+import {reducer} from './reducer';
 
 const init = () => {
+  const api = createAPI((...args) => store.dispatch(...args));
   const store = createStore(
       reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      compose(
+        applyMiddleware(thunk.withExtraArgument(api)),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
   );
 
   ReactDOM.render(
