870172d1333b68ba7765addc5fde2e976aa37970
Vitaly Krupin <vitalykrupin@gmail.com>
2019-06-05 10:30:09 +0500

    comment mocks

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -1,72 +1,72 @@
-export const offers = [
-  {
-    city: {
-      name: `Paris`,
-      coords: [1, 3],
-    },
-    title: `Beautiful, luxurious apartment at great location`,
-    type: `Apartment`,
-    coords: [52.3909553943508, 4.85309666406198],
-    image: `img/apartment-01.jpg`,
-    price: `120`,
-    rate: 93,
-    isBookmarked: true,
-    isPremium: true
-  },
-  {
-    city: {
-      name: `Amsterdam`,
-      coords: [48.8351, 2.3425],
-    },
-    title: `Wood and stone place`,
-    type: `Private room`,
-    coords: [52.369553943508, 4.85309666406198],
-    image: `img/room.jpg`,
-    price: `80`,
-    rate: 80,
-    isBookmarked: false,
-    isPremium: false
-  },
-  {
-    city: {
-      name: `Cologne`,
-      coords: [150, 2],
-    },
-    title: `Canal View Prinsengracht`,
-    type: `Apartment`,
-    coords: [52.3909553943508, 4.929309666406198],
-    image: `img/apartment-02.jpg`,
-    price: `132`,
-    rate: 80,
-    isBookmarked: true,
-    isPremium: false
-  },
-  {
-    city: {
-      name: `Berlin`,
-      coords: [11, 16],
-    },
-    title: `Nice, cozy, warm big bed apartment`,
-    type: `Apartment`,
-    coords: [52.3809553943508, 4.939309666406198],
-    image: `img/apartment-03.jpg`,
-    price: `180`,
-    rate: 100,
-    isBookmarked: false,
-    isPremium: true
-  },
-  {
-    city: {
-      name: `Cologne`,
-      coords: [150, 2],
-    },
-    title: `Canal View Prinsengracht`,
-    type: `Apartment`,
-    coords: [52.3909553943508, 4.929309666406198],
-    image: `img/apartment-02.jpg`,
-    price: `132`,
-    rate: 80,
-    isBookmarked: true,
-    isPremium: false
-  }
-];
+// export const offers = [
+//   {
+//     city: {
+//       name: `Paris`,
+//       coords: [1, 3],
+//     },
+//     title: `Beautiful, luxurious apartment at great location`,
+//     type: `Apartment`,
+//     coords: [52.3909553943508, 4.85309666406198],
+//     image: `img/apartment-01.jpg`,
+//     price: `120`,
+//     rate: 93,
+//     isBookmarked: true,
+//     isPremium: true
+//   },
+//   {
+//     city: {
+//       name: `Amsterdam`,
+//       coords: [48.8351, 2.3425],
+//     },
+//     title: `Wood and stone place`,
+//     type: `Private room`,
+//     coords: [52.369553943508, 4.85309666406198],
+//     image: `img/room.jpg`,
+//     price: `80`,
+//     rate: 80,
+//     isBookmarked: false,
+//     isPremium: false
+//   },
+//   {
+//     city: {
+//       name: `Cologne`,
+//       coords: [150, 2],
+//     },
+//     title: `Canal View Prinsengracht`,
+//     type: `Apartment`,
+//     coords: [52.3909553943508, 4.929309666406198],
+//     image: `img/apartment-02.jpg`,
+//     price: `132`,
+//     rate: 80,
+//     isBookmarked: true,
+//     isPremium: false
+//   },
+//   {
+//     city: {
+//       name: `Berlin`,
+//       coords: [11, 16],
+//     },
+//     title: `Nice, cozy, warm big bed apartment`,
+//     type: `Apartment`,
+//     coords: [52.3809553943508, 4.939309666406198],
+//     image: `img/apartment-03.jpg`,
+//     price: `180`,
+//     rate: 100,
+//     isBookmarked: false,
+//     isPremium: true
+//   },
+//   {
+//     city: {
+//       name: `Cologne`,
+//       coords: [150, 2],
+//     },
+//     title: `Canal View Prinsengracht`,
+//     type: `Apartment`,
+//     coords: [52.3909553943508, 4.929309666406198],
+//     image: `img/apartment-02.jpg`,
+//     price: `132`,
+//     rate: 80,
+//     isBookmarked: true,
+//     isPremium: false
+//   }
+// ];
